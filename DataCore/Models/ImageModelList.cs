b33fe453ae3c8b3c305483241a92369bc4be9e71
omar10120-admin <amr.dawoodi@hotmail.com>
2024-08-21 09:35:08 +0300

    update modify image store

---
+++ DataCore/Models/ImageModelList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataCore.Models
+{
+    public class ImageModelList
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
