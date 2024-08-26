e04d9925d93df24f537ca7da71cc08cc3036d657
omar10120-admin <amr.dawoodi@hotmail.com>
2024-08-27 00:25:42 +0300

    update buy proudct under modify

--- MobileStoreV2/MauiProgram.cs
+++ MobileStoreV2/MauiProgram.cs
@@ -17,8 +17,8 @@ namespace MobileStoreV2
                 });
 
 
-            //   var connectionString = $"Server={Environment.MachineName}\\{Environment.UserName};Database=MobileStore;Trusted_Connection=True;TrustServerCertificate=True;";
-            var connectionString = $"Server={Environment.MachineName};Database=MobileStore;Trusted_Connection=True;TrustServerCertificate=True;";
+             var connectionString = $"Server={Environment.MachineName}\\{Environment.UserName};Database=MobileStore;Trusted_Connection=True;TrustServerCertificate=True;";
+            //   var connectionString = $"Server={Environment.MachineName};Database=MobileStore;Trusted_Connection=True;TrustServerCertificate=True;";
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
